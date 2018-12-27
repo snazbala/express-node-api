82e70e5dc3779b642e11b03fb3688b23110c8836
Sahar Bala <sahar.bala@gmail.com>
2018-12-27 13:51:04 -0800

    First version of note taking api

---
+++ app/routes/note_routes.js
@@ -0,0 +1,82 @@
+const ObjectId = require('mongodb').ObjectId;
+
+module.exports = (app, db) => {
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { _id: new ObjectId(id) };
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({ error: 'An error has occurred on GET' });
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    app.post('/notes', (req, res) => {
+        const note = {
+            text: req.body.text,
+            title: req.body.title,
+        };
+        db.collection('notes').insertOne(note, (err, result) => {
+            if (err) {
+                res.send({ error: 'An error has occurred on POST' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { _id: new ObjectId(id) };
+        db.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                res.send({ error: 'An error has occurred on DELETE' });
+            } else {
+                res.send(`Note ${id} was deleted!`);
+            }
+        });
+    });
+
+    app.put('/notes/:id', (req, res) => {
+        // TODO: only update fields if included in the request
+
+        const id = req.params.id;
+        const details = { _id: new ObjectId(id) };
+
+        const note = {
+            text: req.body.text,
+            title: req.body.title,
+        };
+
+        db.collection('notes').update(details, note, (err, result) => {
+            if (err) {
+                res.send({ error: 'An error has occurred on PUT' });
+            } else {
+                res.send(note);
+            }
+        });
+    });
+
+    app.patch('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { _id: new ObjectId(id) };
+
+        const note = {};
+        if (req.body.text) {
+            note.text = req.body.text;
+        }
+        if (req.body.title) {
+            note.title = req.body.title;
+        }
+
+        db.collection('notes').update(details, { $set: note }, (err, result) => {
+            if (err) {
+                res.send({ error: 'An error has occured on PATCH' });
+            } else {
+                res.send(note);
+            }
+        });
+    });
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('./config/db');
+
+const port = 5500;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(
+    db.url,
+    { useNewUrlParser: true },
+    (err, database) => {
+        if (err) return console.log(err);
+
+        myDb = database.db('notes');
+        require('./app/routes')(app, myDb);
+
+        app.listen(port, () => {
+            console.log(`We are live on ${port}`);
+        });
+    }
+);
